ac1243c25956f5a06c745c69e87d0c9672c911e1
Savannah Kolb <savannahnkolb@gmail.com>
2022-08-16 11:55:50 -0400

    started resume, need to loop through description

--- src/scenes/Home/Resume.js
+++ src/scenes/Home/Resume.js
@@ -1,63 +1,64 @@
 import React from "react";
 import { Container, Row, Col } from "react-grid-system";
 // Components
-import ProgressBar from "../../components/common/ProgressBar";
+// import ProgressBar from "../../components/common/ProgressBar";
 
-const skills = [
-    {
-        title: "Web Design",
-        percent: 100,
-    },
-    {
-        title: "Mobile Apps",
-        percent: 90,
-    },
-    {
-        title: "Graphic Design",
-        percent: 75,
-    },
-    {
-        title: "Creativity",
-        percent: 90,
-    },
-    {
-        title: "Design Creation",
-        percent: 75,
-    },
-    {
-        title: "Marketing online",
-        percent: 90,
-    },
-];
+// const skills = [
+//     {
+//         title: "Web Design",
+//         percent: 100,
+//     },
+//     {
+//         title: "Mobile Apps",
+//         percent: 90,
+//     },
+//     {
+//         title: "Graphic Design",
+//         percent: 75,
+//     },
+//     {
+//         title: "Creativity",
+//         percent: 90,
+//     },
+//     {
+//         title: "Design Creation",
+//         percent: 75,
+//     },
+//     {
+//         title: "Marketing online",
+//         percent: 90,
+//     },
+// ];
 const education = [
     {
-        title: "Art University",
-        subtitle: "Art Director Course | New York",
-        description: "Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo.",
-        date: "(2016-2018)",
-        icon: "ti-crown",
+        title: "Coding: Full Stack Web Development Certificate",
+        subtitle: "University of North Carolina at Chapel Hill",
+        description: "A rigorous curriculum that ensured proficiency in: HTML5, CSS3, JavaScript, jQuery, Express.js, React.js, Node.js, progressive web apps, agile methodology, computer science, database theory, MongoDB, MySQL, Git, and more.",
+        date: "2021",
+        icon: "ti-mouse-alt",
     },
     {
-        title: "Royal Holloway",
-        subtitle: "Bachelor Degree | London",
-        description: "Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo.",
-        date: "(2016-2018)",
-        icon: "ti-cup",
+        title: "Bachelor of Arts in Liberal Studies & Humanities",
+        subtitle: "California State University, San Bernardino",
+        description: "A multidisciplinary degree program designed for students whose needs or interests require a more broadly based liberal arts curriculum than is possible through existing disciplines. This program is useful for careers requiring a broad educational background and strong communication, computer, research, and critical thinking skills.",
+        date: "2013",
+        icon: "ti-marker",
     },
     {
-        title: "Stanford University",
-        subtitle: "Web Design Course | New York",
-        description: "Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo.",
-        date: "(2016-2018)",
+        title: "NC Teacher License",
+        subtitle: "Mathematics 6-9",
+        description: "",
+        date: "2018",
         icon: "ti-ruler-alt-2",
     },
 ];
 const experience = [
     {
-        title: "UI/UX Designer",
-        subtitle: "Google INC | New York",
-        description: "Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo.",
-        date: "(2016-2018)",
+        title: "Front End Engineer",
+        subtitle: "DSD Partners | Richmond, VA",
+        description: ["Using Angular 12, Angular Material, and AWS to create new and update current UI/UX for current clients", "Works directly with clients on developing new applications for their specific needs", "Collaborate with the dev team on different permutations to find the best solution possible", "Works in an agile environment",
+        ],
+        date: "2021 - Present",
         icon: "ti-pencil-alt",
     },
     {
@@ -104,10 +105,10 @@ function Resume() {
                                                         <span className={`el-icon-icon ${item.icon}`}></span>
                                                     </div>
                                                     <h5>{item.title}</h5>
-                                                    <h6>{item.subtitle}</h6>
+                                                    <h6>{item.subtitle} | {item.date}</h6>
                                                 </div>
                                                 <div className="resume-body">
-                                                    <p>Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo.</p>
+                                                    <p>{item.description}</p>
                                                 </div>
                                             </div>
                                         </li>
@@ -128,7 +129,7 @@ function Resume() {
                                                     </div>
                                                     <h5>{item.title}</h5>
                                                     <h6>
-                                                        {item.subtitle} <small>{item.date}</small>
+                                                        {item.subtitle} | <small>{item.date}</small>
                                                     </h6>
                                                 </div>
                                                 <div className="resume-body">
@@ -142,7 +143,7 @@ function Resume() {
                         </Col>
                     </Row>
                 </Container>
-                <Container>
+                {/* <Container>
                     <Row>
                         {skills.map((item, index) => (
                             <Col key={index} sm={6} xl={4}>
@@ -152,7 +153,7 @@ function Resume() {
                             </Col>
                         ))}
                     </Row>
-                </Container>
+                </Container> */}
             </div>
         </section>
     );
