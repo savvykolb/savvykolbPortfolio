7110156945d94e0705eeda20b1cb3083f60bd089
Savannah Kolb <savannahnkolb@gmail.com>
2022-08-17 16:08:07 -0400

    updated contact page

--- src/scenes/Home/Contact.js
+++ src/scenes/Home/Contact.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Container, Hidden, Row, Col } from "react-grid-system";
 
+import { BsFillEnvelopeOpenFill, BsPhone } from "react-icons/bs"
+import { FaLinkedin, FaGithub } from 'react-icons/fa'
+
 function Contact() {
     return (
         <section className="section section-contact section-contact-1">
@@ -10,7 +13,7 @@ function Contact() {
                         <Hidden xs sm md>
                             <Col lg={5}>
                                 <div className="contact-image">
-                                    <div className="image overlay-image" style={{ backgroundImage: `url(/assets/images/contact/1-1.jpg)` }}></div>
+                                    <div className="image overlay-image" style={{ backgroundImage: `url(/assets/images/contact/contactMe.png)` }}></div>
                                 </div>
                             </Col>
                         </Hidden>
@@ -21,33 +24,32 @@ function Contact() {
                                         <span className="el-icon-title">CONTACT ME</span>
                                     </h2>
                                     <h3>
-                                        <span>Get in touch with me today.</span>
+                                        <span>Interested in working together?</span>
                                     </h3>
                                     <div className="divider divider-1"></div>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim enim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                    <br></br>
+                                    <br></br>
+                                    <p>Let's connect and create something cool!</p>
                                 </header>
                                 <form className="form form-1 pt-15">
                                     <Row className="row">
-                                        <Col xs={12} sm={12} md={6}>
-                                            <div className="form-item">
-                                                <input type="text" id="form-item-name" />
-                                                <label htmlFor="form-item-name">Your Name</label>
-                                            </div>
-                                        </Col>
-                                        <Col xs={12} sm={12} md={6}>
-                                            <div className="form-item">
-                                                <input type="email" id="form-item-email" />
-                                                <label htmlFor="form-item-email">Your Email</label>
-                                            </div>
-                                        </Col>
                                         <Col xs={12} sm={12} md={12}>
                                             <div className="form-item">
-                                                <textarea id="form-item-message"></textarea>
-                                                <label htmlFor="form-item-message">Your Message</label>
+                                                <div class="social-links">
+                                                    <br></br>
+                                                    <a target='_blank' rel="noopener noreferrer" href="tel:336-707-0007" class="phoneNumber"><BsPhone /></a>
+                                                    <a target='_blank' rel="noopener noreferrer" href="mailto:savannahnkolb@gmail.com" class="email"><BsFillEnvelopeOpenFill /></a>
+                                                    <a target='_blank' rel="noopener noreferrer" href="https://www.linkedin.com/in/savvykolb/" class="linkedin"><FaLinkedin /></a>
+                                                    <a target='_blank' rel="noopener noreferrer" href="https://github.com/snk923" class="gitHub"><FaGithub /></a>
+                                                    <br></br>
+                                                    <br></br>
+                                                    <br></br>
+                                                    <br></br>
+                                                    <br></br>
+                                                    <br></br>
+                                                    <br></br>
+                                                </div>
                                             </div>
-                                        </Col>
-                                        <Col xs={12} sm={12} md={12}>
-                                            <button className="button button-md button-primary">Send Message</button>
                                         </Col>
                                     </Row>
                                 </form>
