c1d32fccd2a61299df111d9f64d291a65505b220
Savannah Kolb <savannahnkolb@gmail.com>
2022-12-26 15:58:42 -0500

    Updates

---
+++ precache-manifest.50cf42fc59a35065c610950ad907f0f7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91b8a943d15ff1f1ba2a8c08396d88f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77e4afdd09263a7ca6b0",
+    "url": "/static/css/2.366b4564.chunk.css"
+  },
+  {
+    "revision": "ff02838589d44180ef8f",
+    "url": "/static/css/main.96b5fff3.chunk.css"
+  },
+  {
+    "revision": "77e4afdd09263a7ca6b0",
+    "url": "/static/js/2.bfdcdd49.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/static/js/2.bfdcdd49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff02838589d44180ef8f",
+    "url": "/static/js/main.374f53b3.chunk.js"
+  },
+  {
+    "revision": "e9c0041292082624318b",
+    "url": "/static/js/runtime-main.533ece73.js"
+  },
+  {
+    "revision": "23681b800867bc31af423c1618d56a74",
+    "url": "/static/media/S. Kolb Resume .23681b80.pdf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.322b96cff02cebbb937691a1d5fe41ce.js"
+  "/precache-manifest.50cf42fc59a35065c610950ad907f0f7.js"
 );
 
 self.addEventListener('message', (event) => {
